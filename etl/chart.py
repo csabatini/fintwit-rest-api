5503fe3b79fae801f37422d4330ae92e2b807c34
Caleb Sabatini <calebsabatini@gmail.com>
2020-10-06 22:23:32 -0500

    Historical stock data

--- etl/chart.py
+++ etl/chart.py
@@ -3,9 +3,10 @@ import os
 
 def login():
     login = robin_stocks.login(os.environ['RH_USER'], os.environ['RH_PW'])
-    deposits = sum(float(x['amount']) for x in robin_stocks.get_bank_transfers()
-        if (x['direction'] == 'deposit') and (x['state'] == 'completed'))
-    print(deposits)
+    # deposits = sum(float(x['amount']) for x in robin_stocks.get_bank_transfers()
+        # if (x['direction'] == 'deposit') and (x['state'] == 'completed'))
+    data = robin_stocks.stocks.get_stock_historicals("GAN", span='week', bounds='regular')
+    print(data)
 
 if __name__ == '__main__':
     login()
