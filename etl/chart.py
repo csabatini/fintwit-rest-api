2b8e60cac53012a2acb451402a46c8597668a6ea
Caleb Sabatini <calebsabatini@gmail.com>
2020-10-07 23:25:18 -0500

    Format date

--- etl/chart.py
+++ etl/chart.py
@@ -2,6 +2,7 @@ import robin_stocks
 import os
 import json
 import math
+from datetime import datetime
 
 def login():
     login = robin_stocks.login(os.environ['RH_USER'], os.environ['RH_PW'])
@@ -15,13 +16,18 @@ def login():
     summary['high'] = (-1, 0.0)
 
     for i, kv in enumerate(data):
+        begins_at = kv['begins_at']
         low = float(kv['low_price'])
         high = float(kv['high_price'])
         if low < summary['low'][1]:
             summary['low'] = (i, low)
         if high > summary['high'][1]:
             summary['high'] = (i, high)
-    print(json.dumps(summary))
+        date = datetime.strptime(begins_at, '%Y-%m-%dT%H:%M:%SZ')
+        fmt_date = '{}/{}'.format(date.month, date.day)
+        print(fmt_date)
+    # print(json.dumps(summary))
+    # print(data)
 
 if __name__ == '__main__':
     login()
