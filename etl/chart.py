9c33b6f7a92928d2c742b109dee054655b083cad
Caleb Sabatini <calebsabatini@gmail.com>
2020-10-08 00:19:43 -0500

    Format chart url

--- etl/chart.py
+++ etl/chart.py
@@ -10,8 +10,8 @@ def login():
         # if (x['direction'] == 'deposit') and (x['state'] == 'completed'))
     data = robin_stocks.stocks.get_stock_historicals("GAN", span='week', bounds='regular')
     summary = {}
-    summary['first'] = data[0]['close_price']
-    summary['last'] = data[-1]['close_price']
+    summary['first'] = round(float(data[0]['close_price']), 2)
+    summary['last'] = round(float(data[-1]['close_price']), 2)
     summary['low'] = (-1, math.inf)
     summary['high'] = (-1, 0.0)
     summary['last_xl'] = None
@@ -41,22 +41,19 @@ def login():
     # data point labels 
     low = summary['low']
     high = summary['high']
-    chl[low[0]] = str(low[1])
-    chl[high[0]] = str(high[1])
-    if low[0] > 2:
-        chl[2] = summary['first']
-    chl[-1] = summary['last']
+    chl[low[0]] = str(round(low[1], 2))
+    chl[high[0]] = str(round(high[1], 2))
+    if low[0] > 4:
+        chl[2] = str(summary['first'])
+    chl[-1] = str(summary['last'])
 
-    # print(json.dumps(summary))
+    # axis bounds
     lower_bound = (math.floor(summary['low'][1] * 0.95))
     upper_bound = (math.floor(summary['high'][1] * 1.05))
-    # print(lower_bound)
-    # print(upper_bound)
-    print('|'.join(xl))
-    print(','.join(chd))
-    print('|'.join(chl))
 
-    chart_url = "https://image-charts.com/chart?cht=lc&chxt=x,y&chxl=0:|10/1||||||10/2||||||10/5||||||10/6||||||10/7||||&chd=a:16.330000,16.390000,16.170000,16.240000,16.135000,15.970000,16.190000,15.980000,16.070000,16.005000,16.125000,16.200000,18.650000,18.115000,18.390000,18.435000,18.410000,18.200000,18.150000,18.000000,18.150000,18.190000,17.980000,17.580000,17.730000,17.608400,17.650000,17.630000&chl=||16.33||||||||||18.65|||||||||||||||17.68&chco=76A4FB&chls=2.0&chs=480x240&chxr=1,15,20&chlps=offset,5|align,left"
+    chart_url = "https://image-charts.com/chart?cht=lc&chxt=x,y&chxl=0:|{}&chd=a:{}&chl={}&chco=76A4FB&chls=2.5&chs=480x240&chxr=1,{},{}&chlps=offset,5|align,left".\
+        format('|'.join(xl), ','.join(chd), '|'.join(chl), lower_bound, upper_bound)
+    print(chart_url)
 
 if __name__ == '__main__':
     login()
