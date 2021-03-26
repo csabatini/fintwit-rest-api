0500c59ab71805e39ad2a4a2c73ee0135717e418
Caleb Sabatini <calebsabatini@gmail.com>
2021-03-25 19:33:34 -0500

    Bloomberg filter

--- etl/parse.py
+++ etl/parse.py
@@ -61,6 +61,8 @@ def parse_status(status):
         return None
     elif status.user.screen_name == 'CNBCnow' and ('BREAKING' not in parsed_txt or (not url or not media_urls)):
         return None
+    elif status.user.screen_name == 'markets' and not media_urls:
+        return None
     elif status.user.screen_name == 'LiveSquawk' and (not re.findall("\$[A-Z]{1,}", parsed_txt) and not media_urls):
         return None
     elif status.user.screen_name == 'SquawkCNBC' and 'LISTEN' in parsed_txt:
